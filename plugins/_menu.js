11745cdd8c8ee8612981e28c342c3d99d5988a88
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2023-03-30 17:16:45 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,1 +1,137 @@
-const _0x3e858a=_0x2cd4;function _0x2cd4(_0x4d8f15,_0x4c2174){const _0x4bd79f=_0x4bd7();return _0x2cd4=function(_0x2cd416,_0x45285){_0x2cd416=_0x2cd416-0x1d6;let _0x426f10=_0x4bd79f[_0x2cd416];return _0x426f10;},_0x2cd4(_0x4d8f15,_0x4c2174);}(function(_0x365eb6,_0x4060df){const _0x7ed494=_0x2cd4,_0x33e254=_0x365eb6();while(!![]){try{const _0x41e085=-parseInt(_0x7ed494(0x201))/0x1+parseInt(_0x7ed494(0x1ec))/0x2+parseInt(_0x7ed494(0x1ff))/0x3*(parseInt(_0x7ed494(0x203))/0x4)+-parseInt(_0x7ed494(0x1f5))/0x5+parseInt(_0x7ed494(0x1e9))/0x6+parseInt(_0x7ed494(0x1d9))/0x7*(-parseInt(_0x7ed494(0x1f0))/0x8)+-parseInt(_0x7ed494(0x1fb))/0x9;if(_0x41e085===_0x4060df)break;else _0x33e254['push'](_0x33e254['shift']());}catch(_0x4f4f5e){_0x33e254['push'](_0x33e254['shift']());}}}(_0x4bd7,0xb5de5));const events=require('../lib/event'),{FancyRandom,jslbuffer}=require(_0x3e858a(0x1d7)),{command,isPrivate,tiny,serif_B,clockString}=require(_0x3e858a(0x1e6)),{OWNER_NAME,BOT_NAME,FOOTER,HANDLERS}=require(_0x3e858a(0x204)),config=require(_0x3e858a(0x1fc)),prefix=config[_0x3e858a(0x1e7)],{hostname,uptime}=require('os'),thumb=_0x3e858a(0x1fd);command({'pattern':_0x3e858a(0x1fa),'fromMe':isPrivate,'desc':_0x3e858a(0x1e5),'dontAddCommandList':!![]},async(_0x1959e2,_0x1eb523)=>{const _0xf6e7b3=_0x3e858a;if(_0x1eb523)for(let _0x5da2c2 of events[_0xf6e7b3(0x1dd)]){if(_0x5da2c2[_0xf6e7b3(0x1f9)][_0xf6e7b3(0x1e3)](_0x1959e2[_0xf6e7b3(0x1f6)]+_0x1eb523))_0x1959e2[_0xf6e7b3(0x209)]('```Command\x20:\x20'+mssage[_0xf6e7b3(0x1f6)]+_0x1eb523['trim']()+'\x0a\x0aDescription\x20:\x20'+_0x5da2c2[_0xf6e7b3(0x20c)]+_0xf6e7b3(0x1f2));}else{let _0x35f9d5=await jslbuffer(thumb),{prefix:_0x3376d4}=_0x1959e2,[_0x36d78a,_0x4986ea]=new Date()[_0xf6e7b3(0x20b)](_0xf6e7b3(0x1da),{'timeZone':_0xf6e7b3(0x20f)})['split'](','),_0x4a0f8a=_0xf6e7b3(0x1d6)+BOT_NAME+_0xf6e7b3(0x1f3)+OWNER_NAME+'\x0a\x0a┃\x20✗\x20*PREFIX*\x20:\x20'+HANDLERS+_0xf6e7b3(0x1e2)+_0x1959e2['pushName']+_0xf6e7b3(0x205)+hostname()[_0xf6e7b3(0x1f7)]('-')[0x0]+'\x0a\x0a┃\x20✗\x20*DATE*\x20:\x20'+_0x36d78a+'\x0a\x0a┃\x20✗\x20*TIME*\x20:\x20'+_0x4986ea+_0xf6e7b3(0x1df),_0x4f6217=[],_0x44a843,_0x4e97f5=[];return events[_0xf6e7b3(0x1dd)][_0xf6e7b3(0x202)]((_0x935b36,_0x32c03f)=>{const _0x1b2297=_0xf6e7b3;_0x935b36['pattern']&&(_0x44a843=_0x935b36[_0x1b2297(0x1f9)][_0x1b2297(0x1ea)]()[_0x1b2297(0x1dc)](/(\W*)([A-Za-züşiğ öç1234567890]*)/)[0x2]);if(!_0x935b36[_0x1b2297(0x20d)]&&_0x44a843!==undefined){let _0xb6d24f;!_0x935b36[_0x1b2297(0x1e0)]?_0xb6d24f=_0x1b2297(0x1ef):_0xb6d24f=_0x935b36[_0x1b2297(0x1e0)][_0x1b2297(0x1de)]();_0x4f6217[_0x1b2297(0x1ee)]({'cmd':_0x44a843,'type':_0xb6d24f});if(!_0x4e97f5[_0x1b2297(0x1e4)](_0xb6d24f))_0x4e97f5[_0x1b2297(0x1ee)](_0xb6d24f);}}),_0x4f6217[_0xf6e7b3(0x1f4)](),_0x4e97f5[_0xf6e7b3(0x1f4)]()[_0xf6e7b3(0x20a)](_0x6675be=>{const _0x3e0141=_0xf6e7b3;_0x4a0f8a+='\x20\x0a\x0a┃┏━━━━━━━━━━━━━◆\x0a\x0a┣┃\x20*❃-\x20'+_0x6675be+_0x3e0141(0x20e);let _0x3fe9f7=_0x4f6217['filter'](({type:_0xe880d2})=>_0xe880d2==_0x6675be);_0x3fe9f7['forEach'](({cmd:_0x471229},_0x402856)=>{const _0x4f0ca5=_0x3e0141;_0x4a0f8a+=_0x4f0ca5(0x1e8)+(_0x402856+=0x1)+'\x20'+_0x471229[_0x4f0ca5(0x1e1)]();}),_0x4a0f8a+='\x0a┃┗━━━━━━━━━━━━ᐧᐧᐧᐧ⦿\x0a┃';}),_0x4a0f8a+=_0xf6e7b3(0x207),await _0x1959e2[_0xf6e7b3(0x1d8)][_0xf6e7b3(0x208)](_0x1959e2[_0xf6e7b3(0x1db)],{'image':{'url':'https://i.imgur.com/cJnn1gU.png'},'caption':tiny(_0x4a0f8a),'footer':tiny(''+config[_0xf6e7b3(0x1ed)]),'contextInfo':{'externalAdReply':{'title':_0xf6e7b3(0x1f8),'body':'ᴄʀᴇᴀᴛᴇᴅ\x20ʙy\x20ꜱᴜʜᴀɪᴅ\x20🎈','mediaType':0x2,'thumbnail':_0x35f9d5,'mediaUrl':'https://www.instagram.com/______suhaid','sourceUrl':'https://www.instagram.com/______suhaid','showAdAttribution':!![]}}},{'quoted':_0x1959e2});}}),command({'pattern':_0x3e858a(0x200),'fromMe':isPrivate,'desc':'Show\x20All\x20commands','dontAddCommandList':!![]},async(_0x418028,_0xf7213c,{prefix:_0xe21add})=>{const _0x2a0a53=_0x3e858a;let _0x5f0241=_0x2a0a53(0x1eb)+BOT_NAME+_0x2a0a53(0x206),_0x242da4=[],_0x1f9a51,_0x57aa83;return events[_0x2a0a53(0x1dd)][_0x2a0a53(0x202)](_0x886ad5=>{const _0x10529c=_0x2a0a53;_0x886ad5[_0x10529c(0x1f9)]&&(_0x1f9a51=_0x886ad5[_0x10529c(0x1f9)]['toString']()['match'](/(\W*)([A-Za-züşiğ öç1234567890]*)/)[0x2]),_0x886ad5['desc']?_0x57aa83=_0x886ad5[_0x10529c(0x20c)]:_0x57aa83=![],!_0x886ad5[_0x10529c(0x20d)]&&_0x1f9a51!==undefined&&_0x242da4['push']({'cmd':_0x1f9a51,'desc':_0x57aa83});}),_0x242da4['sort'](),_0x242da4[_0x2a0a53(0x20a)](({cmd:_0x528272,desc:_0x1cf88e},_0x169ac9)=>{const _0x37996e=_0x2a0a53;_0x5f0241+=_0x37996e(0x1fe)+(_0x169ac9+=0x1)+'\x20*'+_0x528272[_0x37996e(0x1e1)]()+'*\x0a';}),_0x5f0241+=_0x2a0a53(0x1f1),await _0x418028[_0x2a0a53(0x209)](_0x5f0241);});function _0x4bd7(){const _0x5bdbda=['desc','dontAddCommandList','\x20-✯*\x0a\x0a┃┗━━━━━━━━━━━━━◆\x0a\x0a┃\x0a┃┏━━━━━━━━━━━ᐧᐧᐧᐧ⦿','Asia/Kolkata','┏━━━━━⟪\x20','abu-bot','client','1625792ynwQTo','en-IN','jid','match','commands','toLowerCase','\x0a\x0a┗━━━━━━━━━━━━━━━⦿\x0a\x0a┏━━━『\x20*UPDATED*\x20』━━━❖\x0a╽','type','trim','\x0a\x0a┃\x20✗\x20*USER*\x20:\x20','test','includes','Show\x20All\x20commands','../lib','PREFIX','\x0a┃┃❀\x20','8369862DsSkno','toString','┏━━━━━━━━━━━━━━━━━━━┓\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','2620802ZCvxln','FOOTER','push','misc','32ZRVGPi','┗━━━━━━━━━━━━━━━━━━━','```','\x20⟫━━━⦿\x0a\x0a┃\x20✗\x20*OWNER*\x20:\x20\x20','sort','4308480qkwrhc','prefix','split','${config.THEME}','pattern','menu','12927798jigxKr','../config.js','https://i.imgur.com/E4JrhtY.png','┃×\x20','9TIyawa','list','197855aqlmeF','map','1952736RlMcht','../config','\x0a\x0a┃\x20✗\x20*HOST\x20NAME*\x20:\x20','\x0a\x0a┗━━━━━━━━━━━━━━━━━━━┛\x0a','\x0a┗━━━━━━━━━━━╾ᐧᐧᐧᐧ⦿','sendMessage','reply','forEach','toLocaleString'];_0x4bd7=function(){return _0x5bdbda;};return _0x4bd7();}
+const events = require("../lib/event");
+const { FancyRandom, jslbuffer } = require ("abu-bot");
+const { command, isPrivate, tiny, serif_B, clockString } = require("../lib");
+const { OWNER_NAME, BOT_NAME,FOOTER,THEME, HANDLERS } = require("../config");
+const config = require("../config.js");
+const prefix = config.PREFIX
+const { hostname, uptime } = require("os");
+const thumb = "https://i.imgur.com/E4JrhtY.png";
+command(
+  {
+    pattern: "menu",
+    fromMe: isPrivate,
+    desc: "Show All commands",
+    dontAddCommandList: true,
+  },
+  async (message, match) => {
+    if (match) {
+      for (let i of events.commands) {
+        if (i.pattern.test(message.prefix + match))
+          message.reply(
+            `\`\`\`Command : ${mssage.prefix}${match.trim()}
+Description : ${i.desc}\`\`\``
+          );
+      }
+    } else {
+    let logo = await jslbuffer(thumb)
+      let { prefix } = message;
+      let [date, time] = new Date()
+        .toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+        .split(",");
+      let menu = `┏━━━━━⟪ ${BOT_NAME} ⟫━━━⦿
+┃ ✗ *OWNER* :  ${OWNER_NAME}
+┃ ✗ *PREFIX* : ${HANDLERS}
+┃ ✗ *USER* : ${message.pushName}
+┃ ✗ *HOST NAME* : ${hostname().split("-")[0]}
+┃ ✗ *DATE* : ${date}
+┃ ✗ *TIME* : ${time}
+┗━━━━━━━━━━━━━━━⦿
+┏━━━『 *UPDATED* 』━━━❖\n╽`;
+      let cmnd = [];
+      let cmd;
+      let category = [];
+      events.commands.map((command, num) => {
+        if (command.pattern) {
+          cmd = command.pattern
+            .toString()
+            .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+        }
+
+        if (!command.dontAddCommandList && cmd !== undefined) {
+          let type;
+          if (!command.type) {
+            type = "misc";
+          } else {
+            type = command.type.toLowerCase();
+          }
+
+          cmnd.push({ cmd, type: type });
+
+          if (!category.includes(type)) category.push(type);
+        }
+      });
+      cmnd.sort();
+      category.sort().forEach((cmmd) => {
+       menu += ` 
+┃┏━━━━━━━━━━━━━◆
+┣┃ *❃- ${cmmd} -✯*
+┃┗━━━━━━━━━━━━━◆
+┃\n┃┏━━━━━━━━━━━ᐧᐧᐧᐧ⦿`;    
+        let comad = cmnd.filter(({ type }) => type == cmmd);
+        comad.forEach(({ cmd }, num) => {
+          menu += `\n┃┃❀ ${(num += 1)} ${cmd.trim()}`;
+        });
+        menu += `\n┃┗━━━━━━━━━━━━ᐧᐧᐧᐧ⦿\n┃`;
+      });             
+
+
+      menu += `\n┗━━━━━━━━━━━╾ᐧᐧᐧᐧ⦿`;
+      return await message.client.sendMessage(message.jid, {
+      image: { url: 'https://i.imgur.com/cJnn1gU.png', },
+      caption: tiny(menu),
+      footer: tiny(
+             `${config.FOOTER}`),
+    
+    contextInfo: {
+				externalAdReply: {
+					title:  "${config.THEME}",
+					body: "ᴄʀᴇᴀᴛᴇᴅ ʙy ꜱᴜʜᴀɪᴅ 🎈",
+					mediaType: 2,
+					thumbnail: logo,
+					mediaUrl: 'https://www.instagram.com/______suhaid',
+					sourceUrl: 'https://www.instagram.com/______suhaid',
+					showAdAttribution: true
+					}
+				}
+			}, {quoted: message})
+			}
+})
+
+
+command(
+  {
+    pattern: "list",
+    fromMe: isPrivate,
+    desc: "Show All commands",
+    dontAddCommandList: true,
+  },
+  async (message, match, { prefix }) => {
+    let menu = `┏━━━━━━━━━━━━━━━━━━━┓
+                  ${BOT_NAME}
+┗━━━━━━━━━━━━━━━━━━━┛\n`;
+
+    let cmnd = [];
+    let cmd, desc;
+    events.commands.map((command) => {
+      if (command.pattern) {
+        cmd = command.pattern
+          .toString()
+          .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+      } 
+      if (command.desc) {
+        desc = command.desc;
+      } else {
+        desc = false;
+      }
+      if (!command.dontAddCommandList && cmd !== undefined) {
+        cmnd.push({ cmd, desc });
+      }
+    });
+    cmnd.sort();
+    cmnd.forEach(({ cmd, desc }, num) => {
+      menu += `┃× ${(num += 1)} *${cmd.trim()}*\n`;
+    });
+    menu += `┗━━━━━━━━━━━━━━━━━━━`;
+    return await message.reply(menu);
+  }
+);
