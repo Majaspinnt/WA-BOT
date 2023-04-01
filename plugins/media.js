98a38e1216b660bd8328039bbf1608c767f8a988
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2023-04-01 08:34:28 +0530

    Insta

--- plugins/media.js
+++ plugins/media.js
@@ -250,15 +250,15 @@ command(
   },
   async (message, match) => {
    match = match || message.reply_message.text;
-    if (!match) return await message.sendMessage("_Give Me insta reel Link_");
+    if (!match) return await message.sendMessage("ᴇɴᴛᴇʀ ʟɪɴᴋ");
     
     if (!match.includes("https://www.instagram.com"))
       return await message.reply("_Invalid URL_");
-   messages.reply("*Downloading*");   
+       message.reply("*Downloading...*");
     let response = await getJson(
       `https://api-viper-x0.vercel.app/api/insta?url=${match}`
     );
-    try { message.sendFromUrl(response.media.url); } catch { message.sendMessage("ᴏᴏᴘs !! sᴏᴍᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ 🥴"); }
+    try { message.sendFromUrl(response.media.url); } catch { message.sendMessage("_Error!!_"); }
   }
 );
 
