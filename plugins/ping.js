dfd8adde2de6b1d1edd3925c3c1d5487f405b088
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2023-04-04 18:20:54 +0530

    Ping

--- plugins/ping.js
+++ plugins/ping.js
@@ -7,18 +7,15 @@ Afiya-md
 */
 
 command(
-  {
-    pattern: "ping",
-    fromMe: isPrivate,
-    desc: "To check ping",
-    type: "user",
-  },
-  async (message, match) => {
-    const start = new Date().getTime();
-    await message.sendMessage("```PING```!");
-    const end = new Date().getTime();
-    return await message.sendMessage(
-      "```PONG!``` " + (end - start) + " ᴍs"
-    );
-  }
-);
+	{
+		pattern: 'ping',
+		isOwner: false,
+		desc: "Measures your ping.", 
+		type: 'misc'
+},
+async (conn, match) => {
+	var start = new Date().getTime();
+	await conn.sendMessage(conn.chatId, {text:'```Ping!```'});
+	var end = new Date().getTime();
+	await conn.reply('*Pong!*\n```' + (end - start) + 'ms```');
+	});
