39d02d0ca3e45dd05f7c34b484add28df7f19578
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2023-04-05 17:01:14 +0530

    Fix

--- plugins/media.js
+++ plugins/media.js
@@ -1,4 +1,4 @@
-const {
+
   Function,
   command,
   qrcode,
@@ -6,7 +6,6 @@ const {
   isPublic,
   isUrl,
   isPrivate,
-  getJson,
   getUrl,
   isIgUrl,
   findMusic,
@@ -237,28 +236,6 @@ command(
     let buff = await m.quoted.download();
     buff = await toAudio(buff, "mp3");
     return await message.sendMessage(buff, { mimetype: "audio/mpeg" }, "audio");
-  }
-);
-
-//message.reply_message.text
-command(
-  {
-    pattern: "insta ?(.*)",
-    fromMe: isPrivate,
-    desc: "downloads video from instagram",
-    type: "downloader",
-  },
-  async (message, match) => {
-   match = match || message.reply_message.text;
-    if (!match) return await message.sendMessage("ᴇɴᴛᴇʀ ʟɪɴᴋ");
-    
-    if (!match.includes("https://www.instagram.com"))
-      return await message.reply("_Invalid URL_");
-       message.reply("*Downloading...*");
-    let response = await getJson(
-      `https://api-viper-x0.vercel.app/api/insta?url=${match}`
-    );
-    try { message.sendFromUrl(response.media.url); } catch { message.sendMessage("_Error!!_"); }
   }
 );
 
